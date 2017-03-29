d7a148d0de9fac9fbad8c2efbd61382e25072a72
Solene Bary <solene.bary@gmail.com>
2017-03-29 17:42:24 +0100

    Construction de la page services
    Construction de la page services

--- SITE-WEB/maquette/travel.js
+++ SITE-WEB/maquette/travel.js
@@ -1,8 +1,6 @@
 //création du caroussel pour les photos de voyage
 var $carou1 = $('#carou1');
 var $carou2 = $('#carou2');
-var $flecheG = $('#fleche1');
-var $flecheD = $('#fleche2');
 var $fleche = $('.fleche');
 $(function($) {
 	$carou2.hide();
@@ -13,4 +11,66 @@ $(function($) {
 	});
 
 });
+
+//Page services, gestion du hover pour coeur
+
+$(function ($) {
+  var $avionInit = $('#avionVisible');
+  var $avion = $('#avion');
+  var $avionHover = $('#avionHover');
+  $avionHover.hide();
+  $avionInit.show();
+
+  $avion.mouseenter(function(){
+    $avionInit.hide("slow");
+    $avionHover.show("slow");
+
+    $avion.mouseleave(function(){
+      $avionInit.show("slow");
+      $avionHover.hide("slow");
+    });
+  });
+  
+});
+
+$(function ($) {
+  var $coeurInit = $('#coeurVisible');
+  var $coeur = $('#coeur');
+  var $coeurHover = $('#coeurHover');
+  
+  $coeurHover.hide();
+  $coeurInit.show();
+
+  $coeur.mouseenter(function(){
+    $coeurInit.hide("slow");
+    $coeurHover.show("slow");
+
+    $coeur.mouseleave(function(){
+      $coeurInit.show("slow");
+      $coeurHover.hide("slow");
+    });
+  });
+  
+});
+$(function ($) {
+  var $etoileInit = $('#etoileVisible');
+  var $etoile = $('#etoile');
+  var $etoileBis = $('#etoileHover');
+  
+  $etoileBis.hide();
+  $etoileInit.show();
+
+  $etoile.mouseenter(function(){
+    $etoileInit.hide("slow");
+    $etoileBis.show("slow");
+
+    $etoile.mouseleave(function(){
+      $etoileInit.show("slow");
+      $etoileBis.hide("slow");
+    });
+
+  });
+  
+});
+
 
