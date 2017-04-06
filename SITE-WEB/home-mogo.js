5e93e60dbf9391a532295b8ad53a78c1fc80476c
Solene Bary <solene.bary@gmail.com>
2017-04-06 09:59:10 +0100

    work on the work section and contact and numbers

--- SITE-WEB/home-mogo.js
+++ SITE-WEB/home-mogo.js
@@ -31,6 +31,51 @@ $(document).ready(function() {
     });
 });
 
+//Section numbers pour animer mes chiffres
+
+// Set the date we're counting down to
+var startDate = new Date("Jan 13 15:15:15 2017");
+
+// Update the count down every 1 day
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date();
+
+  // Find the distance between now an the count down date
+  var distanceSec = (now - startDate)/1000 ;
+
+  // Time calculations for weeks, days, hours
+  var weeks = Math.floor(distanceSec/ (60 * 60 * 24 * 7));
+  var days = Math.floor( distanceSec/ (60 * 60 * 24) - weeks*2);
+  var hours = Math.floor(days*7);
+
+  // Display the result in the element with id
+  document.getElementById("weeks").innerHTML = weeks;
+  document.getElementById("coffee").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+
+  // If the count down is finished, write some text 
+ /* if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }*/
+}, 1000);
+
+
+
+//animation des cercles dans la section skills
+window.onload = function onLoad() {
+    var circle = new ProgressBar.Circle('.cercle', {
+        color: '#FCB3B3',
+        duration: 2000,
+        easing: 'bounce',
+        strokeWidth: 6,
+        trailWidth: 1,
+    });
+
+    circle.animate(1);
+};
 
 
 
