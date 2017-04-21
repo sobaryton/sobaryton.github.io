555659b7feb1eb2e3c986a7928236962ecfd532a
Solene Bary <solene.bary@gmail.com>
2017-04-21 20:22:32 +0100

    waves fixed

--- SITE-WEB/home-mogo.js
+++ SITE-WEB/home-mogo.js
@@ -1,26 +1,11 @@
 
-
 //******************************************************Navigation bar on top
-
-$(document).ready(function() {
-	$(window).scroll(function () {
-		var larg = (window.innerWidth);
-		if(larg>767){
-			updateNavigation();
-		}
-	});
-	var larg = (window.innerWidth);
-	if(larg>767){
-		updateNavigation();
-	}
-
-});
 
 function updateNavigation() {
 	var scroll = $(window).scrollTop();
 	var win_height = $("header").height();
-	var $myNavBar = $('.navbar');
-	var $navElt = $('.navbar-left a');
+	var $myNavBar = $(".navbar");
+	var $navElt = $(".navbar-left a");
 	var heightClient = win_height - scroll;
 	if (heightClient <= 0) {
 		$myNavBar.css({"background-color": "#FCB3B3"});
@@ -31,15 +16,34 @@ function updateNavigation() {
 	}
 }
 
+//******************************************************Count the number of wave to put on bottom header
+
+function createWave() {
+	var $crans = $('#crans');
+	var $windowwidth = $("header").width();
+	var $cranswidth = 0;
+
+	while ($cranswidth <= $windowwidth) {
+		var wave = document.createElement("div");
+		var $wave = $(wave);
+		$wave.addClass("triangle-up");
+		$crans.append($wave);
+		var $wavewidth = 40;
+		$cranswidth = $cranswidth + $wavewidth;
+	}
+}
+
+
+
 //******************************************************Animation of the numbers in numbers section
 
-// Set the date we're counting down to
-var startDate = new Date("Jan 13 15:15:15 2017");
+function updateStats() {
+	// Set the date we're counting down to
+	var startDate = new Date("Jan 13 15:15:15 2017");
 
-// Update the count down every 1 day
+	// Update the count down every 1 day
 
-
-	// Get todays date and time
+	// Get today's date and time
 	var now = new Date();
 
 	// Find the distance between now an the count down date
@@ -51,52 +55,51 @@ var startDate = new Date("Jan 13 15:15:15 2017");
 	var hours = Math.floor(days * 7);
 
 	// Display the result in the element with id
-	document.getElementById("weeks").innerHTML = weeks;
-	document.getElementById("coffee").innerHTML = days;
-	document.getElementById("hours").innerHTML = hours;
-
+	document.getElementById("weeks").innerHTML = String(weeks);
+	document.getElementById("coffee").innerHTML = String(days);
+	document.getElementById("hours").innerHTML = String(hours);
+}
 
 
 //***********************************************************************Animation in the skills section
- $(window).scroll(function(){
- var scroll = $( window ).scrollTop();
- var height = $( "header" ).height() + $("#intro").height() + $('#container-numbers').height();
- var heightClient = height-scroll;
- var $animdevDev = $(".animdevDev");
- var $animdevDes = $("#animdevDes");
- var $animdevHum = $("#animdevHum");
 
- //for xs format
- var heightXsDesign = $( "header" ).height() + $("#intro").height() + $('#container-numbers').height() + $("#devskills").height();
- var heightXsHuman = heightXsDesign + $("#designskills").height();
- var heightClientDesign = heightXsDesign-scroll;
- var heightClientHuman = heightXsHuman-scroll;
- if (heightClientDesign <= 0){
-	$animdevDes.css({"animation": "type 4s steps(60, end)"}, {"-webkit-animation": "type 4s steps(60, end)"});
- }
- if (heightClientHuman <=0){
- 	$animdevHum.css({"animation": "type 4s steps(60, end)"}, {"-webkit-animation": "type 4s steps(60, end)"});
- }
- // for the other formats (sm, md, lg)
-	 if (heightClient <= 0){
-		 $animdevDev.css({"animation": "type 4s steps(60, end)"}, {"-webkit-animation": "type 4s steps(60, end)"});
-	 }
- });
+function animateSkills() {
+	var scroll = $(window).scrollTop();
+	var height = $("header").height() + $("#intro").height() + $("#container-numbers").height();
+	var heightClient = height-scroll;
+	var $animdevDev = $(".animdevDev");
+	var $animdevDes = $("#animdevDes");
+	var $animdevHum = $("#animdevHum");
 
-
-
+	//for xs format
+	var heightXsDesign = $("header").height() + $("#intro").height() + $("#container-numbers").height() + $("#devskills").height();
+	var heightXsHuman = heightXsDesign + $("#designskills").height();
+	var heightClientDesign = heightXsDesign-scroll;
+	var heightClientHuman = heightXsHuman-scroll;
+	if (heightClientDesign <= 0){
+		$animdevDes.css({"animation": "type 4s steps(60, end)"}, {"-webkit-animation": "type 4s steps(60, end)"});
+	}
+	if (heightClientHuman <=0){
+		$animdevHum.css({"animation": "type 4s steps(60, end)"}, {"-webkit-animation": "type 4s steps(60, end)"});
+	}
+	// for the other formats (sm, md, lg)
+	if (heightClient <= 0){
+		$animdevDev.css({"animation": "type 4s steps(60, end)"}, {"-webkit-animation": "type 4s steps(60, end)"});
+	}
+}
 
 //*****************************************************************Management of the comments in the contact section
-var $nom = $('#nom');
-var $mail = $('#mail');
-var $company = $('#company');
-var $message = $('#message');
-var $envoi = $("#envoi");
 
-$(document).ready(function(){
+function initEmailJS() {
 	emailjs.init("user_OsNgtYwXQv1DWsI0vRCnf");
 
+	var $nom = $("#nom");
+	var $mail = $("#mail");
+	var $company = $("#company");
+	var $message = $("#message");
+	var $envoi = $("#envoi");
 	var formContact = $("#form");
+
 	formContact.submit(function(event){
 		event.preventDefault();
 
@@ -106,14 +109,9 @@ $(document).ready(function(){
 
 		$envoi.val("Sending...");
 		emailjs.sendForm(service_id,template_id,"form")
-        /*(new Promise(function (resolve, reject){
-            setTimeout(
-                resolve,
-                5000
-            );
-        }))*/
 			.then(function(){
 				alert("Your comment is sent. Thank you for that I will answer you as soon as possible.");
+
 				formContact.find("button").text("Send");
 
 				$nom.val("");
@@ -123,7 +121,9 @@ $(document).ready(function(){
 
 				$envoi.val("Send");
 			}, function(err) {
-				alert("Send email failed!\r\n Please contact me directly at solene.bary@gmail.com \n Thank you\n Error:\n " + JSON.stringify(err));
+				alert("Send email failed!\r\nPlease contact me directly at solene.bary@gmail.com\nThank you\n");
+				console.log("Error: " + JSON.stringify(err));
+
 				formContact.find("button").text("Send");
 
 				$nom.val("");
@@ -133,8 +133,41 @@ $(document).ready(function(){
 			});
 		return false;
 	})
+}
+
+//**********************************************************************Check what the current page is
+
+function isHome() {
+	var regex = /home-mogo.html/i;
+	return regex.test(window.location.href);
+}
+function isCV() {
+	var regex = /cv.html/i;
+	return regex.test(window.location.href);
+}
+
+//******************************************************************Finally load all functions at the right moment
+
+$(document).ready(function() {
+	if(window.innerWidth > 767) {
+		updateNavigation();
+	}
+
+	if (isHome()) {
+		initEmailJS();
+		updateStats();
+	}
+	if (isCV()) {
+		createWave();
+	}
 });
 
+$(window).scroll(function() {
+	if(window.innerWidth > 767) {
+		updateNavigation();
+	}
 
-
-
+	if (isHome()) {
+		animateSkills();
+	}
+});
