0f8ea85a3a4a7e3d3385ba8bea26be1a42590b06
Solene Bary <solene.bary@gmail.com>
2017-04-24 12:25:55 +0100

    travel - travel ideas page, in xs format

--- SITE-WEBFR/maquette/travel.js
+++ SITE-WEBFR/maquette/travel.js
@@ -1,118 +1,148 @@
 
 //Carousel with the pictures
-var $carou1 = $('#carou1');
-var $carou2 = $('#carou2');
-var $fleche = $('.fleche');
-$(function($) {
-	$carou2.hide();
-	$carou1.show();
-	$fleche.on ("click", function(){
-		$carou2.toggle("fast");
-		$carou1.toggle("fast");
-	});
+//function carousel(){
+	var $carou1 = $('#carou1');
+	var $carou2 = $('#carou2');
+	var $fleche = $('.fleche');
+	$(function($) {
+		$carou2.hide();
+		$carou1.show();
+		$fleche.on ("click", function(){
+			$carou2.toggle("fast");
+			$carou1.toggle("fast");
+		});
 
-});
+	});
+//}
+
+
 
 //Services pages, work on the hover for the plane, heart and star
+//function hoverDiv(){
+	$(function () {
+		var $avionInit = $('#avionVisible');
+		var $avion = $('#avion');
+		var $avionHover = $('#avionHover');
+		$avionHover.hide();
+		$avionInit.show();
 
-$(function () {
-  var $avionInit = $('#avionVisible');
-  var $avion = $('#avion');
-  var $avionHover = $('#avionHover');
-  $avionHover.hide();
-  $avionInit.show();
+		$avion.mouseenter(function(){
+			$avionInit.hide("slow");
+			$avionHover.show("slow");
 
-  $avion.mouseenter(function(){
-    $avionInit.hide("slow");
-    $avionHover.show("slow");
+			$avion.mouseleave(function(){
+				$avionInit.show("slow");
+				$avionHover.hide("slow");
+			});
+		});
 
-    $avion.mouseleave(function(){
-      $avionInit.show("slow");
-      $avionHover.hide("slow");
-    });
-  });
-  
-});
+	});
 
-$(function () {
-  var $coeurInit = $('#coeurVisible');
-  var $coeur = $('#coeur');
-  var $coeurHover = $('#coeurHover');
-  
-  $coeurHover.hide();
-  $coeurInit.show();
+	$(function () {
+		var $coeurInit = $('#coeurVisible');
+		var $coeur = $('#coeur');
+		var $coeurHover = $('#coeurHover');
 
-  $coeur.mouseenter(function(){
-    $coeurInit.hide("slow");
-    $coeurHover.show("slow");
+		$coeurHover.hide();
+		$coeurInit.show();
 
-    $coeur.mouseleave(function(){
-      $coeurInit.show("slow");
-      $coeurHover.hide("slow");
-    });
-  });
-  
-});
-$(function () {
-  var $etoileInit = $('#etoileVisible');
-  var $etoile = $('#etoile');
-  var $etoileBis = $('#etoileHover');
-  
-  $etoileBis.hide();
-  $etoileInit.show();
+		$coeur.mouseenter(function(){
+			$coeurInit.hide("slow");
+			$coeurHover.show("slow");
 
-  $etoile.mouseenter(function(){
-    $etoileInit.hide("slow");
-    $etoileBis.show("slow");
+			$coeur.mouseleave(function(){
+				$coeurInit.show("slow");
+				$coeurHover.hide("slow");
+			});
+		});
 
-    $etoile.mouseleave(function(){
-      $etoileInit.show("slow");
-      $etoileBis.hide("slow");
-    });
+	});
+	$(function () {
+		var $etoileInit = $('#etoileVisible');
+		var $etoile = $('#etoile');
+		var $etoileBis = $('#etoileHover');
 
-  });
-  
-});
+		$etoileBis.hide();
+		$etoileInit.show();
+
+		$etoile.mouseenter(function(){
+			$etoileInit.hide("slow");
+			$etoileBis.show("slow");
+
+			$etoile.mouseleave(function(){
+				$etoileInit.show("slow");
+				$etoileBis.hide("slow");
+			});
+
+		});
+
+	});
+
+//}
+
 
 //Page About us, management of the pictures with a width smaller than 500px
-var $trust = $('#trust');
-var $quality = $('#quality');
-var $safety = $('#safety');
-var $trustimg = $('#trustimg');
-var $qualityimg = $('#qualityimg');
-var $safetyimg = $('#safetyimg');
-var larg = (window.innerWidth);
+//function resizePictures(){
+	var $trust = $('#trust');
+	var $quality = $('#quality');
+	var $safety = $('#safety');
+	var $trustimg = $('#trustimg');
+	var $qualityimg = $('#qualityimg');
+	var $safetyimg = $('#safetyimg');
+	var larg = (window.innerWidth);
 
-if ( larg <500) {
-  $trustimg.hide();
-  $qualityimg.hide();
-  $safetyimg.hide();
+	if ( larg <500) {
+		$trustimg.hide();
+		$qualityimg.hide();
+		$safetyimg.hide();
 
-  $trust.css("margin-bottom:40px");
-  $safety.css("margin-bottom:40px");
-  $quality.css("margin-bottom:40px");
-}
+		$trust.css("margin-bottom:40px");
+		$safety.css("margin-bottom:40px");
+		$quality.css("margin-bottom:40px");
+	}
+//}
+
+
 
 
 //Page travel ideas, on xs and sm formats, when you click on the country, price, a programm and a "book" button appear.
 
 if ( larg <767){
-	var $tripprix = $('.tripprix');
+	var $details = $('.details');
 	var $prixdetail = $('.prixdetail');
 	$prixdetail.hide();
-	$tripprix.each( function(index){
-		var $tripprixindex = $('#tripprix'+index);
-		var $prixdetailindex = $('#prixdetail'+index);
-		$tripprixindex.click(function() {
-			$tripprixindex.hide();
-			$prixdetailindex.slideToggle("slow");
-			$prixdetailindex.click(function () {
-				$prixdetailindex.slideUp("slow");
-				$tripprixindex.show();
-			});
-		});
+	$details.click( function(){
+
+		$tripprix = $($(this).children()[0]);
+		$prixdetailpays = $($(this).children()[1]);
+
+		$prixdetailpays.slideToggle("slow");
+		$tripprix.toggle("slow");
+
 	});
 }
+
+
+//**********************************************************************Check what the current page is
+/*
+function isHome() {
+	var regex = /index.html/i;
+	return regex.test(window.location.href);
+}
+function isAboutUs() {
+	var regex = /aboutus.html/i;
+	return regex.test(window.location.href);
+}
+function isServices() {
+	var regex = /services.html/i;
+	return regex.test(window.location.href);
+}
+function isTravelIdeas() {
+	var regex = /travelideas.html/i;
+	return regex.test(window.location.href);
+}*/
+//******************************************************************Finally load all functions at the right moment
+
 
 
 
